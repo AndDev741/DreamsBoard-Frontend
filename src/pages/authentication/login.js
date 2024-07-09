bd9c8590ca4a2f810373df8b99eafb4a853b8e86
AndDev <anddesenvolved@gmail.com>
2024-07-09 17:16:06 -0300

    Login page UI created

---
+++ src/pages/authentication/login.js
@@ -0,0 +1,73 @@
+import loginPC from '../../assets/loginPC.png';
+
+function Login(){
+    return(
+        <div className="flex flex-col lg:flex-row items-center justify-center bg-[#EEFCC7] lg:bg-white min-h-screen">
+            <div className='flex flex-col lg:flex-row-reverse items-center justify-center w-full h-full'>
+                
+                <div className='flex flex-col items-center justify-center lg:w-[550px] lg:h-[580px] lg:bg-[#EEFCC7] lg:rounded-r-lg'>
+                    <div className="mt-2 text-redFont text-4xl md:text-5xl lg:text-5xl font-black text-center">
+                        <h1 className="">Bem vindo</h1>
+                        <h1>de volta!</h1>
+                    </div>
+
+                    <div className="mt-3 text-lightRed font-medium text-3xl md:text-4xl lg:text-3xl text-center">
+                        <h3>Crie e gerencie seus</h3>
+                        <h3>quadros de sonhos</h3>
+                    </div>
+
+                    <form className='flex flex-col items-center justify-center'>
+                        <div className="mt-5">
+                            <label htmlFor="email" 
+                            className="text-ligthGray text-xl md:text-3xl">Email</label>
+                            <br/>
+                            <input name="email"
+                            id="email"
+                            type="text"
+                            className="bg-[#EEEBEA] border border-lightRed rounded-lg w-[80vw] lg:w-[450px] h-[60px] md:h-[70px] lg:h-[60px] text-2xl md:text-3xl pl-4
+                            focus:ring-redFont focus:border-redFont"
+                            placeholder="email@gmail.com"/>
+                        </div>
+                        <div className="mt-5 md:mt-5">
+                            <label htmlFor="password" 
+                            className="text-ligthGray text-xl md:text-3xl">Password</label>
+                            <br/>
+                            <input name="password"
+                            id="password"
+                            type="password"
+                            className="bg-[#EEEBEA] border border-lightRed rounded-lg w-[80vw] lg:w-[450px] h-[60px] md:h-[70px] lg:h-[60px] text-2xl md:text-3xl pl-4
+                            focus:ring-redFont focus:border-redFont"
+                            />
+                        </div>
+                        <div className="flex items-center justify-center mt-5">
+                            <div>
+                                <input type="submit"
+                                id="submit"
+                                name="submit"
+                                value="Entrar"
+                                className="bg-redFont hover:bg-lightRed
+                                w-[207px] md:w-[307px] lg:w-[250px] h-[51px] md:h-[70px] lg:h-[60px]
+                                text-white text-xl md:text-3xl font-bold rounded-lg cursor-pointer"/>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center mt-3">
+                            <p className="text-redFont text-xl md:text-3xl underline cursor-pointer">Registre-se</p>
+                        </div>
+                    </form>
+                </div>
+
+                <div className='flex lg:flex-col items-center justify-between lg:w-[550px] lg:h-[580px] lg:bg-redFont lg:rounded-l-lg'>
+                    <img src={loginPC}
+                    alt="homan in a playground"
+                    className={'w-[100vw]'}/>
+                    <p className='text-white font-black text-3xl text-center w-[470px] mb-12 hidden lg:block'>A melhor maneira de gerir seus sonhos</p>
+                </div>
+
+            </div>
+        </div>
+
+        
+    )
+}
+
+export default Login;
---
+++ src/pages/authentication/renderForms.js
@@ -0,0 +1,11 @@
+import Login from "./login"
+
+function RenderForms(){
+    return(
+        <div className="h-[100%]">
+            <Login/>
+        </div>
+    )
+}
+
+export default RenderForms;
