6874506f5b4ac5b9a4d34d2f48834f4ff6656222
AndDev <anddesenvolved@gmail.com>
2024-08-08 14:56:08 -0300

    newBoard path created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './pages/authentication/login.js'
 import Login from './pages/authentication/login.js';
 import Register from './pages/authentication/register.js';
 import Dashboard from './pages/dashboard/dashboard';
-import NewBoard from './pages/dashboard/newBoard';
+import NewBoard from './pages/board/newBoard/newBoard';
 
 function App() {
   return (
