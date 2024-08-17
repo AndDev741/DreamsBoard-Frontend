4e73bb458b2a9117e63fd2e833a4c5418a7976bf
AndDev <anddesenvolved@gmail.com>
2024-08-17 11:56:39 -0300

    New error messages in edit user component

--- src/pages/perfilConfiguration/perfilConfiguration.js
+++ src/pages/perfilConfiguration/perfilConfiguration.js
@@ -6,10 +6,13 @@ import logoutIcon from '../../assets/logout.png';
 import axios from "axios";
 import customAxios from '../../axiosConfig';
 import EditPerfil from "./editPerfil";
+import { img_linkEnter, nameEnter, perfil_phraseEnter } from "../authentication/loginSlice";
 
 function PerfilConfiguration(){
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
 
     const id = useSelector(state => state.login.id);
     const img_link = useSelector(state => state.login.img_link);
@@ -57,13 +60,18 @@ function PerfilConfiguration(){
             }
         }catch(e){
             console.error("Error trying upload perfil photo", e);
+            setError("An error ocurred trying to edit, try again");
         }
 
         try{
             const response = customAxios.put("/user/edit", editData);
-            
+            dispatch(img_linkEnter(perfilImg));
+            dispatch(nameEnter(newName));
+            dispatch(perfil_phraseEnter(newPerfilPhrase));
+            setSuccess((await response).data.success);
         }catch(e){
             console.error("Erro", e);
+            setError("An error ocurred trying to edit, try again");
         }
 
     }
@@ -79,7 +87,7 @@ function PerfilConfiguration(){
                 </button>
                 <Link to={"/dashboard"}>
                     <button className="text-white font-medium w-[180px] h-[40px] bg-redFont rounded-md text-xl hover:bg-lightRed my-2">
-                        Cancel
+                        Go back
                     </button>
                 </Link>
                 <button onClick={handleLogout}
@@ -87,6 +95,8 @@ function PerfilConfiguration(){
                     Logout
                 </button>
             </div>
+            <p className='text-xl text-center text-red-900 font-semibold underline'>{error}</p>
+            <p className='text-xl text-center text-blue-900 font-semibold underline'>{success}</p>
         </div>
     )
 }
