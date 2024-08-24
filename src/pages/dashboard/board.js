e86371e1fb83ae8eec31a05d280398d36b755896
andDev741 <anddesenvolved@gmail.com>
2024-08-24 11:46:34 -0300

    Acessibility change in the board to DreamBoard

--- src/pages/dashboard/board.js
+++ src/pages/dashboard/board.js
@@ -1,6 +1,7 @@
 import { idEnter } from "../board/dreamBoardSlice";
 import { useDispatch, UseDispatch } from "react-redux";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 function Board({id, background_img, title}){
     const dispatch = useDispatch();
@@ -11,13 +12,15 @@ function Board({id, background_img, title}){
         navigate("/dreamBoard");
     }
     return(
-        <div onClick={goToDreamBoard}
-        className="w-[90vw] h-[200px] md:h-[240px] md:w-[500px] rounded-[6px] my-8 cursor-pointer lg:mx-10">
-            <div className={`flex items-center justify-center w-full h-full rounded-[6px] bg-cover hover:border-2 hover:border-yellow-300`}
-            style={{backgroundImage: background_img ? `url(${background_img})` : null}}>
+        <Link onClick={goToDreamBoard} to={"/dreamBoard"}>
+            <div 
+            className="w-[90vw] h-[200px] md:h-[240px] md:w-[500px] rounded-[6px] my-8 cursor-pointer lg:mx-10">
+                <div className={`flex items-center justify-center w-full h-full rounded-[6px] bg-cover hover:border-2 hover:border-yellow-300`}
+                style={{backgroundImage: background_img ? `url(${background_img})` : null}}>
+                </div>
+                <h1 className="text-2xl lg:text-3xl text-center text-[#000] font-extrabold underline mt-2">{title}</h1>
             </div>
-            <h1 className="text-2xl lg:text-3xl text-center text-[#000] font-extrabold underline mt-2">{title}</h1>
-        </div>
+        </Link>
     )
 }
 
